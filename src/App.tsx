0dd757d24e33564c8c538c4ceeab392299884448
Ponk8 <lucas.alencar31@gmail.com>
2023-08-29 23:43:36 -0300

    feat: added loading during user retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import UseAuth from "./services/UseAuth";
+
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import "./index.css";
 
 function App() {
+  const { loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <UseAuth></UseAuth>;
+  }
+
   return (
-    <div className="flex flex-col h-screen bg-neutral-800">
-      <Header />
-      <Outlet />
-    </div>
+      <div className="flex flex-col h-screen bg-neutral-800">
+        <Header />
+        <Outlet />
+      </div>
   );
 }
 
