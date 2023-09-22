62e8747ca7d4e51d51b2e439323062a68773adae
Ponk8 <lucas.alencar31@gmail.com>
2023-09-22 16:18:58 -0300

    feat: Update CartItem values from localStorage and add item modification functions
    - Updated CartItem values to be sourced directly from localStorage.
    - Added functions to update item quantity or delete items from localStorage.
    - Implemented the parent component to update the total cart value.

--- src/components/ShoppingItem.tsx
+++ src/components/ShoppingItem.tsx
@@ -13,18 +13,45 @@ export default function ShoppingItem({
   Stock,
   Price,
 }: ShoppingItemProps) {
+  const addToCart = () => {
+    const cartItem = { ItemName, ItemImage, Stock, Price, quantity: 1 };
+    const existingCartItems = JSON.parse(
+      localStorage.getItem("cartItems") || "[]"
+    );
+
+    const itemIndex = existingCartItems.findIndex(
+      (item) => item.ItemName === cartItem.ItemName
+    );
+
+    if (itemIndex !== -1) {
+      existingCartItems[itemIndex].quantity += 1;
+    } else {
+      existingCartItems.push(cartItem);
+    }
+
+    localStorage.setItem("cartItems", JSON.stringify(existingCartItems));
+
+    const updatedStock = Stock - 1;
+    localStorage.setItem(ItemName + "_stock", updatedStock.toString());
+  };
+
   return (
     <div>
       <div className="bg-neutral-300 rounded-sm flex flex-col justify-center items-center pb-3.5">
-        <div className="font-bold">{ItemName}</div>
-        <img className="lg:h-14 h-12" src={ItemImage} alt="" />
-        <div>stock: {Stock}</div>
-        <div className="flex justify-center items-center">
+        <div className="font-bold text-sm sm:text-base">{ItemName}</div>
+        <div className="p-1 sm:w-[5rem] sm:h-[4rem] w-[3rem] h-[2.5rem]">
+          <img className="w-full h-full object-cover" src={ItemImage} alt="" />
+        </div>
+        <div className="text-xs sm:text-base">stock: {Stock}</div>
+        <div className="flex justify-center items-center text-xs sm:text-base">
           price: {Price}
           <img className="w-[10%] lg:w-[15%]" src={GoldIcon} alt="" />
         </div>
       </div>
-      <button className="bg-amber-300 hover:bg-amber-400 border-neutral-800 border-dashed border-2 w-1/2 flex justify-center items-center mx-auto -mt-3.5">
+      <button
+        onClick={addToCart}
+        className="bg-amber-300 hover:bg-amber-400 border-neutral-800 border-dashed border-2 w-1/2 flex justify-center items-center mx-auto -mt-3.5 text-xs sm:text-base"
+      >
         add to cart
       </button>
     </div>
